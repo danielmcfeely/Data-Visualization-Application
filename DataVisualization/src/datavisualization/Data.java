d39f8d4d84ac87b2675ae46395ba6be48fc38f10
ColinApplebee <ColinApplebee@10.0.0.9>
2018-03-18 20:52:28 -0400

    Added separate values to parse the data.

--- DataVisualization/src/datavisualization/Data.java
+++ DataVisualization/src/datavisualization/Data.java
@@ -3,32 +3,53 @@ package datavisualization;
 
 public class Data {
     private String category;
-    private int value;
+    private int value1;
+    private int value2;
+    private int value3;
     
-    Data(String category, int value) {
+    Data(String category, int value, int value2, int value3) {
     this.category = category;
-    this.value = value;
+    this.value1 = value1;
+    this.value2 = value2;
+    this.value3 = value3;
     }
 
     public String getCategory() {
         return category;
     }
 
-    public int getValue() {
-        return value;
+    public int getValue1() {
+        return value1;
     }
 
     public void setCategory(String category) {
         this.category = category;
     }
 
-    public void setValue(int value) {
-        this.value = value;
+    public void setValue1(int value) {
+        this.value1 = value1;
     }
+
+    public int getValue2() {
+        return value2;
+    }
+
+    public int getValue3() {
+        return value3;
+    }
+
+    public void setValue2(int value2) {
+        this.value2 = value2;
+    }
+
+    public void setValue3(int value3) {
+        this.value3 = value3;
+    }
+    
 
     @Override
     public String toString() {
-        return "Category=" + category + ", Value=" + value + '}';
+        return  category + "," +  value1 + "," + value2 + "," + value3;
     }
     
     
